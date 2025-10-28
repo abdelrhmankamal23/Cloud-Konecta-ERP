a674d0ca6ab43d434c573bade04f5d78b6d7ea5c
mohamed ashraf <mohamed.ashraf65@msa.edu.eg>
2025-10-28 16:06:33 +0300

    Update variables.tf

--- terraform/modules/eks/variables.tf
+++ terraform/modules/eks/variables.tf
@@ -9,43 +9,13 @@ variable "vpc_id" {
 }
 
 variable "private_subnet_ids" {
-  description = "List of private subnet IDs for EKS nodes"
+  description = "List of private subnet IDs for Fargate pods"
   type        = list(string)
 }
 
 variable "public_subnet_ids" {
   description = "List of public subnet IDs for EKS control plane"
   type        = list(string)
-}
-
-variable "node_instance_type" {
-  description = "EC2 instance type for EKS worker nodes"
-  type        = string
-  default     = "t3.medium"
-}
-
-variable "node_desired_size" {
-  description = "Desired number of worker nodes"
-  type        = number
-  default     = 2
-}
-
-variable "node_max_size" {
-  description = "Maximum number of worker nodes"
-  type        = number
-  default     = 4
-}
-
-variable "node_min_size" {
-  description = "Minimum number of worker nodes"
-  type        = number
-  default     = 1
-}
-
-variable "key_name" {
-  description = "EC2 Key Pair name for SSH access to EKS nodes"
-  type        = string
-  default     = ""
 }
 
 variable "vpc_cidr" {
